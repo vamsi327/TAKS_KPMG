b12c01ad2b5f416636eb31fd819c373c2cd853ce
vamsi327 <56817954+vamsi327@users.noreply.github.com>
2021-05-12 23:58:21 +0530

    Create 8-Secuirty-groups.tf

---
+++ 8-Secuirty-groups.tf
@@ -0,0 +1,73 @@
+# ./security.tf
+
+resource "aws_security_group" "general_sg" {
+  description = "HTTP egress to anywhere"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Project = "sdn-tutorial"
+  }
+}
+
+resource "aws_security_group" "bastion_sg" {
+  description = "SSH ingress to Bastion and SSH egress to App"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Project = "sdn-tutorial"
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  description = "SSH ingress from Bastion and all TCP traffic ingress from ALB Security Group"
+  vpc_id      = aws_vpc.main.id
+  tags = {
+    Project = "sdn-tutorial"
+  }
+}
+resource "aws_security_group_rule" "out_http" {
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.general_sg.id
+}
+
+resource "aws_security_group_rule" "out_ssh_bastion" {
+  type                     = "egress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.bastion_sg.id
+  source_security_group_id = aws_security_group.app_sg.id
+}
+
+resource "aws_security_group_rule" "out_http_app" {
+  type              = "egress"
+  description       = "Allow TCP internet traffic egress from app layer"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.app_sg.id
+}
+
+resource "aws_security_group_rule" "in_ssh_bastion_from_anywhere" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
+resource "aws_security_group_rule" "in_ssh_app_from_bastion" {
+  type                     = "ingress"
+  description              = "Allow SSH from a Bastion Security Group"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.app_sg.id
+  source_security_group_id = aws_security_group.bastion_sg.id
+}
